87ba116e8aff517ab795b8813bb3d14330fd2437
Lukas Hermanns <lukasbanana@users.noreply.github.com>
2013-06-18 10:39:32 +0000

    - Completed TextureBuffer for D3D11.

--- tests/TextureBufferTests/TBOShader.hlsl
+++ tests/TextureBufferTests/TBOShader.hlsl
@@ -5,13 +5,22 @@ struct SVertexIn
 {
 	float3 Position : POSITION;
 	float2 TexCoord : TEXCOORD;
+	uint Id			: SV_VertexID;
 };
 
 struct SVertexOut
 {
 	float4 Position : SV_Position;
-	float2 TexCoord : TEXCOORD;
+	float2 TexCoord : TEXCOORD0;
+	float4 Color	: TEXCOORD1;
 };
+
+/*tbuffer BufTex : register(t0)
+{
+	float4 BufferColors[100];
+};*/
+
+Buffer<float4> BufferColors;
 
 cbuffer BufferVertex : register(c0)
 {
@@ -22,8 +31,9 @@ SVertexOut VertexMain(SVertexIn In)
 {
 	SVertexOut Out = (SVertexOut)0;
 	
-	Out.Position = mul(WorldViewProjectionMatrix, float4(In.Position, 1.0));
-	Out.TexCoord = In.TexCoord;
+	Out.Position	= mul(WorldViewProjectionMatrix, float4(In.Position, 1.0));
+	Out.TexCoord	= In.TexCoord;
+	Out.Color		= BufferColors[In.Id];// + BufferColors2[0];
 	
 	return Out;
 };
@@ -31,16 +41,12 @@ SVertexOut VertexMain(SVertexIn In)
 
 // Texture Buffer Object (TBO) Test HLSL Pixel Shader
 
-tbuffer BufTex : register(t0)
-{
-	float4 BufferColors[100];
-};
-
 float4 PixelMain(SVertexOut In) : SV_Target0
 {
 	float4 Color = (float4)0;
 	
-	Color = float4(In.TexCoord.x, In.TexCoord.y, BufferColors[0].x, 1.0);
+	//Color = float4(In.TexCoord.x, In.TexCoord.y, 0.0, 1.0);
+	Color = In.Color;
 	
 	return Color;
 };
