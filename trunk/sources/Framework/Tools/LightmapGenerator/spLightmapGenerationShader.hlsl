9b18346a843c24fd97486c92befc603e9f3f4988
lukasbanana <lukasbanana@cad53b02-d668-c241-bac5-9609d919ae81>
2013-08-29 11:49:08 +0000

    - Bug fix in lightmap-generation compute-shader.

--- trunk/sources/Framework/Tools/LightmapGenerator/spLightmapGenerationShader.hlsl
+++ trunk/sources/Framework/Tools/LightmapGenerator/spLightmapGenerationShader.hlsl
@@ -34,34 +34,60 @@ cbuffer BufferRadiosityRays : register(b2)
 StructuredBuffer<SLightSource> LightList : register(t0);
 StructuredBuffer<SLightmapTexel> Lightmap : register(t1); 	// Active lightmap texels (one draw-call for every lightmap texture)
 
-RWStructuredBuffer<STriangle> TriangleList : register(u2);
-RWStructuredBuffer<SKDTreeNode> NodeList : register(u3);
-RWBuffer<uint> TriangleIdList : register(u4);
+StructuredBuffer<STriangle> TriangleList : register(t2);
+StructuredBuffer<SKDTreeNode> NodeList : register(t3);
+Buffer<uint> TriangleIdList : register(t4);
 
 RWTexture2D<float4> OutputLightmap : register(u0);
+
+groupshared SIdStack Stack;
 
 
 /* === Functions === */
 
 #include "spLightmapGenerationProcs.shader"
 
-[numthreads(8, 8, 1)]
-void ComputeMain(
-	uint3 GroupId : SV_GroupID,
-	uint3 LocalId : SV_GroupThreadID,
-	uint3 GlobalId : SV_DispatchThreadID)
+[numthreads(1, 1, 1)]
+void ComputeDirectIllumination(uint3 Id : SV_DispatchThreadID)
 {
 	float4 Color = AmbientColor;
 	
 	/* Get current lightmap texel */
-	uint2 TexelPos = GlobalId.xy;
+	uint2 TexelPos = Id.xy;
 	SLightmapTexel Texel = Lightmap[TexelPos.y * LightmapSize.x + TexelPos.x];
 	
 	/* Generate lightmap texel for each light source */
-	[allow_uav_condition]
 	for (uint i = 0; i < NumLights; ++i)
 		GenerateLightmapTexel(Color.rgb, LightList[i], Texel);
 	
 	OutputLightmap[TexelPos] = Color;
 }
 
+[numthreads(1, 1, 1)]
+void ComputeIndirectIllumination(uint3 Id : SV_DispatchThreadID)
+{
+	float4 Color = (float4)0.0;
+	
+	/* Get current lightmap texel */
+	uint2 TexelPos = Id.xy;
+	SLightmapTexel Texel = Lightmap[TexelPos.y * LightmapSize.x + TexelPos.x];
+	
+	/* Generate normal matrix (tangent space) */
+	float3x3 NormalMatrix = float3x3(
+		Texel.Tangent,
+		cross(Texel.Normal, Texel.Tangent),
+		Texel.Normal
+	);
+	
+	/* Sample radiosity rays */
+	SRay Ray;
+	Ray.Origin = Texel.WorldPos;
+	
+	for (uint i = 0; i < NumRadiosityRays; ++i)
+	{
+		Ray.Direction = GetRandomRayDirection(NormalMatrix, i);
+		ComputeRadiosityShading(Color, Ray, Texel.Normal);
+	}
+	
+	InterlockedAdd(OutputLightmap[TexelPos], Color);
+}
